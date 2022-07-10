1b4e0ead3956798861581157a37b571822379a44
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2022-07-10 22:47:55 +0530

    done

---
+++ app.js
@@ -0,0 +1,34 @@
+ 
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var output = document.querySelector("#output");
+
+
+var serverURL ="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+
+function errorHandler(error){
+    console.log("some error occured",error);
+    alert("something is wrong with the server please try again later")
+}
+
+function clickHandler(){
+
+   var inputText = txtInput.value;//input
+
+   //processing
+   fetch(getTranslationURL(inputText))
+   .then(response => response.json())
+    
+   .then(json => {
+    var translatedText = json.contents.translated;
+    output.innerText = translatedText;//output
+   })
+   .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click",clickHandler)
