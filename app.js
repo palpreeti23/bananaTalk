f2c96d7476792fe73cd107aec5b1fbc1b2852f74
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2022-09-06 23:39:07 +0530

    update style

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
- 
-var txtInput = document.querySelector("#txt-input");
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 var output = document.querySelector("#output");
 
 
